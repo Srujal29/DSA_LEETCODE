b71a363be6c895fd1b224165f80efd30f6d91057
Srujal29 <srujalvispute2006@gmail.com>
2026-08-16 21:31:00 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (43.65%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
+    /*
+    for every lement if found increase the count else decrease the count whichever the remaing is the answer
+
+    again iterate throh array cnt that element if greater than n/2 then is s the anserr
+    */
     int majorityElement(vector<int>& nums) {
-       unordered_map<int,int>mp;
+        int element;
+        int cnt = 0;
 
-       for(int i=0; i<nums.size();i++){
-        mp[nums[i]]++;
-       } 
-
-       for(auto it : mp){
-        if(it.second > (nums.size() / 2)){
-            return it.first;
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt==0){
+                cnt = 1;
+                element = nums[i];
+            }
+            else if(nums[i] == element){
+                cnt++;
+            }else{
+                cnt--;
+            }
         }
-       }
-       return -1;
+        int count = 0;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] == element){
+                count++;
+            }
+        }
+
+        if(count > (nums.size() / 2))return element;
+        else return -1;
     }
 };
