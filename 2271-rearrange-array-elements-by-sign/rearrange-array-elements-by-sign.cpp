4e07c5e1856910504e040106ea7892a70abf942b
Srujal29 <srujalvispute2006@gmail.com>
2026-08-16 23:58:16 +0530

    Time: 5 ms (64.12%) | Memory: 128.3 MB (53.88%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+/*
+    pos will be at the even position and neg will at odd
+*/
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans(n,0);
+        int posIdx = 0, negIdx = 1;
+        for(int i=0;i <n;i++){
+
+            if(nums[i] < 0){
+                ans[negIdx] = nums[i];
+                negIdx += 2;
+            }else{
+                ans[posIdx] = nums[i];
+                posIdx += 2;
+            }
+        }
+
+        return ans;
+    }
+};
