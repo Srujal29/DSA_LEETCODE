d61db9a7894368f5c4d1a86d78b5626c42aa8a1a
Srujal29 <srujalvispute2006@gmail.com>
2026-09-27 00:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (39.27%) - LeetSync

---
+++ 768-partition-labels/partition-labels.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> last(26,-1);
+        for(int i=0;i<s.size();i++){
+            last[s[i] - 'a'] = i;
+        }
+
+        vector<int> ans;
+        int st = 0, end = 0;
+
+        for(int i=0;i<s.size();i++){
+            end = max(end , last[s[i] - 'a']);
+
+            if(i==end){
+                ans.push_back(end-st + 1);
+                st = i + 1;
+
+            }
+        }
+
+        return ans;
+    }
+};
