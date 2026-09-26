2d4c823f59eaf0d9e4a421b564ec95302178c564
Srujal29 <srujalvispute2006@gmail.com>
2026-09-27 00:25:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (57.09%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int  l = 0;
+        int ans = 0;
+        for(int i = 0 ;i < s.size();i++){
+            if(s[i] == 'L'){
+                l++;
+            }else{
+                l--;
+            }
+
+            if(l == 0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
